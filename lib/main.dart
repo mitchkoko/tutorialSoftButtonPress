27cd73049d19bc97aeed6e2a3545b46e10bd2367
creality <mitchell.h.koh@gmail.com>
2020-01-24 18:08:30 +1100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'button.dart';
 import 'buttontapped.dart';
 
@@ -17,18 +16,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  // SET ICONS HERE
 
   var icons1 = Icons.home;
   var icons2 = Icons.settings;
   var icons3 = Icons.favorite;
   var icons4 = Icons.message;
+
+  //
 
   bool buttonPressed1 = false;
   bool buttonPressed2 = false;
@@ -43,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       buttonPressed4 = false;
     });
   }
+
   void _letsPress2() {
     setState(() {
       buttonPressed1 = false;
@@ -51,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       buttonPressed4 = false;
     });
   }
+
   void _letsPress3() {
     setState(() {
       buttonPressed1 = false;
@@ -59,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       buttonPressed4 = false;
     });
   }
+
   void _letsPress4() {
     setState(() {
       buttonPressed1 = false;
@@ -70,7 +74,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
@@ -78,66 +81,63 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(18),
+              padding: EdgeInsets.all(20),
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: SizedBox(width: 50,),
+                    child: GestureDetector(
+                        // FIRST BUTTON
+                        onTap: _letsPress1,
+                        child: buttonPressed1
+                            ? ButtonTapped(
+                                icon: icons1,
+                              )
+                            : MyButton(
+                                icon: icons1,
+                              )),
                   ),
-
                   Expanded(
                     child: GestureDetector(
-                      onTap: _letsPress2,
-                      child: buttonPressed2 ? ButtonTapped(icon: icons2,) : MyButton(icon: icons2,) ,
-                    ),
+                        // SECOND BUTTON
+                        onTap: _letsPress2,
+                        child: buttonPressed2
+                            ? ButtonTapped(
+                                icon: icons2,
+                              )
+                            : MyButton(
+                                icon: icons2,
+                              )),
                   ),
                   Expanded(
-                    child: SizedBox(width: 50,),
+                    child: GestureDetector(
+                        // THIRD BUTTON
+                        onTap: _letsPress3,
+                        child: buttonPressed3
+                            ? ButtonTapped(
+                                icon: icons3,
+                              )
+                            : MyButton(
+                                icon: icons3,
+                              )),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                        // FOURTH BUTTON
+                        onTap: _letsPress4,
+                        child: buttonPressed4
+                            ? ButtonTapped(
+                                icon: icons4,
+                              )
+                            : MyButton(
+                                icon: icons4,
+                              )),
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 100,),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child:
-
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: GestureDetector(   // FIRST BUTTON
-                      onTap: _letsPress1,
-                      child: buttonPressed1 ? ButtonTapped(icon: icons1,) : MyButton(icon: icons1,)
-                    ),
-                  ),
-                  Expanded(
-                    child: GestureDetector(   // SECOND BUTTON
-                      onTap: _letsPress2,
-                      child: buttonPressed2 ? ButtonTapped(icon: icons2,) : MyButton(icon: icons2,)
-                    ),
-                  ),
-                  Expanded(
-                    child: GestureDetector(   // THIRD BUTTON
-                      onTap: _letsPress3,
-                      child: buttonPressed3 ? ButtonTapped(icon: icons3,) : MyButton(icon: icons3,)
-                    ),
-                  ),
-                  Expanded(
-                    child: GestureDetector(   // FOURTH BUTTON
-                      onTap: _letsPress4,
-                      child: buttonPressed4 ? ButtonTapped(icon: icons4,) : MyButton(icon: icons4,)
-                    ),
-                  ),
-                ],
-              )
-
-              ,
             )
           ],
         ),
-
       ),
     );
-
   }
 }
